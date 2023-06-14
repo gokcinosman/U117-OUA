277512fc8df2376c0f30f28f533bebb9a7ba6909
Osman Gokcin <osmangokcinn@gmail.com>
2023-06-14 20:53:57 +0300

    Udate playermovement

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,17 +4,30 @@ public class PlayerMovement : MonoBehaviour
 {
     public float speed = 5f; // Karakter hızı
     public float rotationSpeed = 100f; // Dönüş hızı
+    public float jumpForce = 5f; // Zıplama kuvveti
+    public Transform groundCheck; // Ground check için kullanılan boş nesne
+    public float groundDistance = 0.2f; // Ground check mesafesi
+    public LayerMask groundLayer; // Zeminin layer'ı
 
     private Rigidbody rb;
     private Quaternion targetRotation;
+    private bool isGrounded;
 
     private void Start()
     {
         rb = GetComponent<Rigidbody>();
-        targetRotation = transform.rotation; // Initialize targetRotation with the initial rotation of the object
+        targetRotation = transform.rotation; // İlk rotasyonu hedef rotasyon olarak ayarla
     }
 
     private void Update()
+    {
+        HandleMovement();
+
+        if (Input.GetKeyDown(KeyCode.Space) && isGrounded)
+            Jump();
+    }
+
+    private void HandleMovement()
     {
         float horizontalInput = Input.GetAxisRaw("Horizontal");
 
@@ -29,6 +42,15 @@ public class PlayerMovement : MonoBehaviour
         float verticalInput = Input.GetAxisRaw("Vertical");
 
         Vector3 movement = transform.forward * verticalInput * speed * Time.deltaTime;
+
         rb.MovePosition(rb.position + movement);
+
+        // Ground check'i yap ve isGrounded değerini güncelle
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundLayer);
+    }
+
+    private void Jump()
+    {
+        rb.AddForce(Vector3.up * jumpForce, ForceMode.Impulse);
     }
 }
