c966ac305d11e22fa3dce176c7a7e88581daf638
Osman Gokcin <osmangokcinn@gmail.com>
2023-06-19 21:23:21 +0300

    crosshair koyuldu

--- Assets/Scripts/MouseLock.cs
+++ Assets/Scripts/MouseLock.cs
@@ -2,34 +2,9 @@ using UnityEngine;
 
 public class MouseLock : MonoBehaviour
 {
-    private bool isMouseLocked = true; // Fare kilitlenmiş olarak başlasın
-
-    private void Update()
+    void Start()
     {
-        if (Input.GetMouseButtonDown(0)) // Fare tıklandığında
-        {
-            isMouseLocked = !isMouseLocked; // Fare kilidini tersine çevir
-
-            if (isMouseLocked)
-            {
-                LockMouse(); // Fareyi kitle
-            }
-            else
-            {
-                UnlockMouse(); // Fare kilidini kaldır
-            }
-        }
-    }
-
-    private void LockMouse()
-    {
-        Cursor.lockState = CursorLockMode.Locked; // Fareyi kitle
-        Cursor.visible = false; // Fareyi görünmez yap
-    }
-
-    private void UnlockMouse()
-    {
-        Cursor.lockState = CursorLockMode.None; // Fare kilidini kaldır
-        Cursor.visible = true; // Fareyi görünür yap
+        Cursor.visible = false;
+        Cursor.lockState = CursorLockMode.Locked;
     }
 }
